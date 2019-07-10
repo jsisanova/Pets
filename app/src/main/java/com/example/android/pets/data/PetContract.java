caa02dbcb01eac96aa5db390a6838e010b4e5d50
jsisanova <jana.sisanova@gmail.com>
2019-07-10 10:21:40 +0200

    Add constants for pet content URIs to Contract

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -1,6 +1,10 @@
 package com.example.android.pets.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
+
+import java.net.URI;
+import java.security.Provider;
 
 /**
  * API Contract for the Pets app.
@@ -12,10 +16,43 @@ public final class PetContract {
     private PetContract() {}
 
     /**
+     * The "Content authority" is a name for the entire content provider, similar to the
+     * relationship between a domain name and its website.  A convenient string to use for the
+     * content authority is the package name for the app, which is guaranteed to be unique on the
+     * device.
+     * Used to help identify the Content Provider which we’d setup in the AndroidManifest tag.
+     */
+    public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+
+    /**
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+     * the content provider.
+     * To make this a usable URI, we use the parse method which takes in a URI string and returns a Uri.
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    /**
+     * This constants store the path for each of the tables which will be appended to the base content URI.
+     * Possible path (appended to base content URI for possible URI's)
+     * For instance, content://com.example.android.pets/pets/ is a valid path for
+     * looking at pet data. content://com.example.android.pets/staff/ will fail,
+     * as the ContentProvider hasn't been given any information on what to do with "staff".
+     */
+    public static final String PATH_PETS = "pets";
+
+    /**
      * Inner class that defines constant values for the pets database table.
      * Each entry in the table represents a single pet.
      */
     public static final class PetEntry implements BaseColumns {
+
+        /**
+         * The content URI to access the pet data in the provider.
+         * Inside each of the Entry classes in the contract, we create a full URI for the class
+         * as a constant called CONTENT_URI. The Uri.withAppendedPath() method appends the BASE_CONTENT_URI
+         * (which contains the scheme and the content authority) to the path segment.
+         */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
 
         /** Name of database table for pets */
         public final static String TABLE_NAME = "pets";
