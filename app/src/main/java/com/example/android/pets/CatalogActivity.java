cf6575b6e9761574669512e78970834012111597
jsisanova <jana.sisanova@gmail.com>
2019-07-13 11:35:10 +0200

    Add empty view to the ListView

--- app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -51,6 +51,13 @@ public class CatalogActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        // Find the ListView which will be populated with the pet data
+        ListView petListView = (ListView) findViewById(R.id.list);
+
+        // Find and set empty view on the ListView, so that it only shows when the list has 0 items.
+        View emptyView = findViewById(R.id.empty_view);
+        petListView.setEmptyView(emptyView);
     }
 
     // When activity starts again, after user clicks Save in editor activity, the list will refresh with new pet in the database
